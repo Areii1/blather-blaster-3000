76d5d0013f7de6a8680b00597b946bdd324e8906
Areii1 <ari.jaaskelainen1@gmail.com>
2017-12-08 02:54:18 +0200

    refactor answerButtonList

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 import YandexTranslate from 'yandex-translate';
+/*import VoiceRSS from './voicerss-tts-min';*/
+import ReactPlayer from 'react-player';
+
 
 import Inputfield from './components/inputField';
 import AnswerButton from './components/answerButton';
@@ -8,7 +10,8 @@ import AnswerButtonList from './components/answerButtonList';
 import ConclusionMessage from './components/conclusionMessage';
 import TranslatedString from './components/translatedString';
 
-import apiKey from './api-key';
+import YandexApiKey from './yandex-api-key';
+import RssApiKey from './rss-api-key';
 
 const importantLangTable = ['Japanese', 'Esperanto', 'Swedish',
 'Estonian', 'Latin', 'Finnish', 'Chinese', 'Greek',
@@ -18,22 +21,64 @@ const importantLangTable = ['Japanese', 'Esperanto', 'Swedish',
 const importantLangKeyTable = ['ja', 'eo', 'sv', 'et', 'la',
 'fi', 'zh', 'el', 'ru', 'de', 'af'];
 
-const yandexInstance = YandexTranslate(apiKey);
+const yandexInstance = YandexTranslate(YandexApiKey);
+
+
+/*DOESTN WORK*/ 
+/*
+const whadup = VoiceRSS.speech({
+  key: RssApiKey,
+  src: 'Hello, world!',
+  hl: 'en-us',
+  r: 0, 
+  c: 'mp3',
+  f: '44khz_16bit_stereo',
+  ssml: false
+});
+*/
+
+/*DOESTN WORK*/ 
+/*
+var mpg321 = require('mpg321');
+ 
+var
+file = './hurroo.mp3',
+player = mpg321().remote();
+ 
+// infinity loop 
+player.play(file);
+player.on('end', function () {
+  console.log('end');
+  player.play(file);
+});
+ 
+// SIGINT hack 
+process.on('SIGINT', function () {
+  process.exit();
+});
+ 
+// SIGINT hack 
+process.on('SIGINT', function (data) {
+  process.exit();
+});
+*/
 
 class App extends Component {
   constructor(props) {
     super(props);
     
     this.state = {
-      gameprocess: 0,
+      gameProcess: 0,
       textFieldValueSubmitted: "",
       resultText: "",
       rightAnswerKey: "",
-      AnswerMessage: ""
+      AnswerMessage: "",
+      player: {}
     }
 
     this.translateText = this.translateText.bind(this);
     this.checkAnswer = this.checkAnswer.bind(this);
+    this.playerRender = this.playerRender.bind(this);
   }
 
   getRandomLang() {
@@ -48,11 +93,13 @@ class App extends Component {
     if (importantLangKeyTable[buttonClicked] === this.state.rightAnswerKey) {
       this.setState({AnswerMessage: "You are absolutely RIGHT >:))), kudos to you my friend"}, () => {
         console.log("inside checkAnswer : rightanswerGiven: " + this.state.rightAnswerGiven);
+        this.setState({gameprocess: 2});
       });
     }
     else {
       this.setState({AnswerMessage: ("NO IT's " + this.state.rightAnswerKey + " you dumbass :D:D:D:")}, () => {
         console.log("inside checkAnswer: wronganswerGiven: " + this.state.wrongAnswerGiven);
+        this.setState({gameprocess: 2});
       });
     }
   }
@@ -64,12 +111,28 @@ class App extends Component {
       yandexInstance.translate(this.state.textFieldValueSubmitted, { to: randLangKey }, (err, res) => {
         this.setState({resultText: res.text});
         this.setState({rightAnswerKey: randLangKey});
-        this.setState({gameprocess: 1});
+        this.setState({gameProcess: 1});
       });
     });
   }
+  
+  playerRender() {
+    console.log("player render: GamePROCESS: " + this.state.gameProcess);
+    if (this.state.gameProcess == 2) {
+      return <ReactPlayer url='https://www.youtube.com/watch?v=4uEnNSclcqIh' playing={true} />;
+    }
+    else return null;
+  }
       
   render() {
+    if (this.state.gameProcess === 1) {
+      this.state.player = <ReactPlayer url='https://www.youtube.com/watch?v=4uEnNSclcqIh' playing={true} />;
+      console.log("react player inside RENDER: " + this.state.player);      
+    }
+    else
+    console.log("react player inside RENDER: " + this.state.player);
+
+    const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     return ( 
       <div>
         <Inputfield 
@@ -78,7 +141,8 @@ class App extends Component {
         <TranslatedString 
          printableTranslation={this.state.resultText}
         />
-        <AnswerButtonList 
+        <AnswerButtonList
+          numbers={numbers}
           label={importantLangTable} 
           checkAnswer={this.checkAnswer}
         />
@@ -91,5 +155,3 @@ class App extends Component {
 }
 
 export default App;
-
-ReactDOM.render(<App />, document.getElementById('root'));
--- src/components/answerButtonList.js
+++ src/components/answerButtonList.js
@@ -1,73 +1,22 @@
-import React, { Component } from 'react';
-
+import React from 'react';
 import AnswerButton from './answerButton';
 
-class AnswerButtonList extends Component {
-  constructor(props) {
-    super(props);
-  }
+function AnswerButtonList(props) {
 
-  render() {
+    const numbers = props.numbers;
+    const listItems = numbers.map((number) =>
+        <AnswerButton 
+          label={props.label[number]}
+          checkAnswer={props.checkAnswer}
+          id={number}
+        />
+    );
+
     return (
       <div>
-        <AnswerButton 
-        label={this.props.label[0]} 
-        checkAnswer={this.props.checkAnswer}
-        id={0}
-        />
-        <AnswerButton 
-        label={this.props.label[1]} 
-        checkAnswer={this.props.checkAnswer}
-        id={1}
-        />
-        <AnswerButton 
-        label={this.props.label[2]} 
-        checkAnswer={this.props.checkAnswer}
-        id={2}
-        />
-        <AnswerButton 
-        label={this.props.label[3]} 
-        checkAnswer={this.props.checkAnswer}
-        id={3}
-        />
-        <AnswerButton 
-        label={this.props.label[4]} 
-        checkAnswer={this.props.checkAnswer}
-        id={4}
-        />
-        <AnswerButton 
-        label={this.props.label[5]} 
-        checkAnswer={this.props.checkAnswer}
-        id={5}
-        />
-        <AnswerButton 
-        label={this.props.label[6]} 
-        checkAnswer={this.props.checkAnswer}
-        id={6}
-        />
-        <AnswerButton 
-        label={this.props.label[7]} 
-        checkAnswer={this.props.checkAnswer}
-        id={7}
-        />
-        <AnswerButton 
-        label={this.props.label[8]} 
-        checkAnswer={this.props.checkAnswer}
-        id={8}
-        />
-        <AnswerButton 
-        label={this.props.label[9]} 
-        checkAnswer={this.props.checkAnswer}
-        id={9}
-        />
-        <AnswerButton 
-        label={this.props.label[10]} 
-        checkAnswer={this.props.checkAnswer}
-        id={10}
-        />
+        {listItems}
       </div>
     );
   }
-}
 
 export default AnswerButtonList;
