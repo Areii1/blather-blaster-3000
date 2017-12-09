b6628d30dbc15d87fb66639e399f20483ac173e5
Areii1 <ari.jaaskelainen1@gmail.com>
2017-12-09 15:34:55 +0200

    refactor answerButton into a functional component

--- src/components/answerButton.js
+++ src/components/answerButton.js
@@ -1,21 +1,13 @@
 import React, { Component } from 'react';
 
-class AnswerButton extends Component {
-  constructor(props) {
-    super(props);
-
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick() {
-    this.props.checkAnswer(this.props.label);
-  }
-  
-  render () {
-    return (
-      <button onClick={this.handleClick}>{this.props.label}</button>
-    );
-  }
+function AnswerButton(props) {
+  return (
+    <button onClick={() => {
+      props.checkAnswer(props.label)
+    }}>
+    {props.label}
+    </button>
+  );
 }
 
 export default AnswerButton;
