7896ce24fba96889fbb3ee37882dd2a3413577a8
Areii1 <ari.jaaskelainen1@gmail.com>
2017-12-09 15:22:56 +0200

    refactor conclusionMessage to functional component

--- src/components/conclusionMessage.js
+++ src/components/conclusionMessage.js
@@ -1,22 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class ConclusionMessage extends Component {
-  render () {
-    return (
+function ConclusionMessage(props) {
+  return (
+    <div>
+      {props.answer && (
+        <h3>You are correct</h3>
+      )}
+      {!props.answer && (
       <div>
-        {this.props.answer && (
-          <h3>You are correct</h3>
-        )}
-        {!this.props.answer && (
-        <div>
-          <h3>You are wrong</h3>
-          <p>that was {this.props.rightAnswerName} actually :) </p>
-        </div>
-        )}
-        <button onClick={this.props.resetGame}>Play again</button>
+        <h3>You are wrong</h3>
+        <p>that was {props.rightAnswerName} actually :) </p>
       </div>
-    );
-  }
+      )}
+      <button onClick={props.resetGame}>Play again</button>
+    </div>
+  );
 }
 
 export default ConclusionMessage;
